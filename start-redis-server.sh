b026815d74f77197bc2651a8204c9e30ba0220e9
Joshua Sierles <joshua@chatterbug.com>
2021-10-07 10:21:37 +0200

    Ensure that Redis binds to ipv6

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sysctl vm.overcommit_memory=1
 sysctl net.core.somaxconn=1024
-redis-server --requirepass $REDIS_PASSWORD --bind 0.0.0.0 --dir /data/ --appendonly yes
+redis-server --requirepass $REDIS_PASSWORD --dir /data/ --appendonly yes
