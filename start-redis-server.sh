1c9f4187b50a8639a103995bbef34ccc2b92d912
Joshua Sierles <joshua@hey.com>
2021-12-07 00:49:59 +0100

    Use default RDB save settings

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -5,4 +5,4 @@ sysctl net.core.somaxconn=1024
 MAXMEMORY=${FLY_VM_MEMORY_MB:-512}
 MAXMEMORY="$(((($FLY_VM_MEMORY_MB*10)-$FLY_VM_MEMORY_MB)/10))"
 
-redis-server --requirepass $REDIS_PASSWORD --dir /data/ --save 60 1000 --maxmemory "${MAXMEMORY}mb"
+redis-server --requirepass $REDIS_PASSWORD --dir /data/ --maxmemory "${MAXMEMORY}mb"
