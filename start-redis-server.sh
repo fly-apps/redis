21d6b83062c25b4185fd34d714b477e9b936d665
Matt-Yorkley <9029026+Matt-Yorkley@users.noreply.github.com>
2022-03-08 12:12:56 +0000

    Allow customising configuration via ENV vars and set some sane defaults

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -5,4 +5,11 @@ sysctl net.core.somaxconn=1024
 MAXMEMORY=${FLY_VM_MEMORY_MB:-512}
 MAXMEMORY="$(((($FLY_VM_MEMORY_MB*10)-$FLY_VM_MEMORY_MB)/10))"
 
-redis-server --requirepass $REDIS_PASSWORD --dir /data/ --maxmemory "${MAXMEMORY}mb"
+: ${MAXMEMORY_POLICY:="allkeys-lru"}
+: ${APPENDONLY:="no"}
+
+redis-server --requirepass $REDIS_PASSWORD \
+  --dir /data/ \
+  --maxmemory "${MAXMEMORY}mb" \
+  --maxmemory-policy $MAXMEMORY_POLICY \
+  --appendonly $APPENDONLY
