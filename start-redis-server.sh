abb32894145036e7c6d745403738e9fa2eadfba7
Joshua Sierles <joshua@fly.io>
2022-07-08 00:00:40 +0200

    Fix start script

--- start-redis-server.sh
+++ start-redis-server.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
-sysctl vm.overcommit_memory=1
-sysctl net.core.somaxconn=1024
 
-MAXMEMORY=${FLY_VM_MEMORY_MB:-512}
-MAXMEMORY="$(((($FLY_VM_MEMORY_MB*10)-$FLY_VM_MEMORY_MB)/10))"
+set -e
 
-: ${MAXMEMORY_POLICY:="allkeys-lru"}
+sysctl vm.overcommit_memory=1 || true
+sysctl net.core.somaxconn=1024 || true
+
+if [[ -z "${REDIS_PASSWORD}" ]]; then
+  echo "The REDIS_PASSWORD environment variable is required"
+  exit 1
+fi
+
+# Set maxmemory-policy to 'allkeys-lru' for caching servers that should always evict old keys
+: ${MAXMEMORY_POLICY:="volatile-lru"}
 : ${APPENDONLY:="no"}
+: ${FLY_VM_MEMORY_DB:=512}
+
+# Set maxmemory to 10% of available memory
+MAXMEMORY=$(($FLY_VM_MEMORY_DB*90/100))
 
 redis-server --requirepass $REDIS_PASSWORD \
   --dir /data/ \
